f7043ff7f3285da60ab508ac93df78c944af91a4
Sheyranyann <139764356+Sheyranyann@users.noreply.github.com>
2024-01-19 21:21:41 +0400

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package System.Management.Bookstore;
-
-public class Main {
-    public static void main(String[] args) {
-        BookStore bookStore = new BookStore();
-        bookStore.management();
-    }
-}
