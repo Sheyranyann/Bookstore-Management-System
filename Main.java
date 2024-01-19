07f81ad5df24f6be2835a1285a0bbe2616b8d4a1
Sheyranyann <139764356+Sheyranyann@users.noreply.github.com>
2024-01-19 20:57:34 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package System.Management.Bookstore;
+
+public class Main {
+    public static void main(String[] args) {
+        BookStore bookStore = new BookStore();
+        bookStore.management();
+    }
+}
